243d2b40e6c9ffb837474c80f1cf28fffdbc98d6
siimhuts <siimhuts@greeny.cs.tlu.ee>
2016-09-14 09:34:44 +0300

    lisasin kella ja kuup2va

--- helloWorld.php
+++ helloWorld.php
@@ -8,3 +8,40 @@
 	
 	echo $firstName." ".$lastName;
 ?>
+
+<br>
+
+<?php
+
+	$age = 21;
+	
+	if ($age < 18) {
+		
+		// kui vastus on tõene (true)
+		echo "Alaealine";
+	} else {
+		// kui vastus on vale (false)
+		echo "Täisealine";
+		
+	}
+?>
+
+<br>
+
+<?php
+
+	for($i = 1; $i <= $age; $i = $i + 1)
+	{
+		echo $i."Palju ";
+	}
+	
+	echo "õnne";
+?>
+
+<br>
+
+<?php
+
+	echo date("D, d M Y H:i:s");
+
+?>
